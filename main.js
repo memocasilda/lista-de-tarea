639184392735e644942b5ee34249b9d1371189f5
memocasilda <memocasilda@gmail.com>
2025-04-16 16:56:45 -0300

    cambio js

--- main.js
+++ main.js
@@ -4,17 +4,44 @@ const mas = document.getElementById("agregar");
 console.log(agregar);
 const lista = document.getElementById("lista2");
 
-const agregarTarea = (tarea) => {
+const hecho = 'fa-check-circle';
+const pendiente = 'fa-circle';
+const tachado = 'tachado';
+
+
+const agregarTarea = (tarea, check) => {
+    const estado = check ? hecho : pendiente;
+    const tachar = check ? tachado : "";
+
     const elemento = `<li>
-                    <i class="fas fa-circle co check" id="check" data-="check"></i>
-                    <p class="tarea tachado" id="tarea">${tarea}</p>
-                    <i class="fas fa-trash de borrar" id="borrar" data-="borrar"></i>
+                    <i class="far ${estado} check" id="check" data="check"></i>
+                    <p class="tarea ${tachar}" id="tarea">${tarea}</p>
+                    <i class="fas fa-trash de borrar" id="borrar" data="borrar"></i>
                 </li>`
                 lista.insertAdjacentHTML("beforeend", elemento);
 };
 
+const tareaRealizada = (element) => {
+    element.classList.toggle (hecho);
+    element.classList.toggle (pendiente);
+    element.parentNode.querySelector('.tarea').classList.toggle(tachado);
+    };
 
-// llamas funcion
+    const tareaEliminada = (element) => {
+        element.parentNode.parentNode.removeChild(element.parentNode);
+
+        
+
+    };
+
+    const cambiarEstilo = () => {
+        const link = document.getElementById("style");
+        link.href = link.href.includes("style.css") ? "style2.css" : "style.css";
+    
+    };
+
+
+// llamar funciones
 
 mas.addEventListener('click', () => {
 const tarea = input.value 
@@ -38,9 +65,17 @@ input.value = "";
 
 )
 
-const cambiarEstilo = () => {
-    const link = document.getElementById("style");
-    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";
+lista.addEventListener('click', function (event){
+    const elementData = element.atributes.data.value;
+    if (elementData == 'check'){
+        tareaRealizada (element)
+    }
 
-};
+    else if (elementData =='borrar'){
+        tareaEliminada (element)
+    }
+
+}
+)
+
 
